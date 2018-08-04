2b15e16c51c436152249bd1df49fbfa2c42f7eeb
Lucas Lyra <lucalyra@gmail.com>
2018-08-04 16:51:06 +0300

    Bot commands only on his channel

--- bot.js
+++ bot.js
@@ -9,14 +9,17 @@ client.on("ready", () => {
   let configUpdate = require(`./commands/inside/installConfig.js`);
   configUpdate.run(client);
 
-console.log("I am ready!");
+  console.log("I am ready!");
 });
 
 client.on("message", (message) => {
-  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   let isAdmin = (message.author.id == message.guild.ownerID );
+  console.log(message.channel.id)
+  if(message.content == "!install"){let commandFile = require(`./commands/install.js`); commandFile.run(client, message , args, isAdmin)}
+  if (!message.content.startsWith(config.prefix) || message.author.bot || message.channel.id !== config.botChannel) {return};
+
 
   try {
     let commandFile = require(`./commands/${command}.js`);
